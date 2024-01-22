2463fa4e8b7275b005290f2c2120b0160f5e3019
Rohit <rohitkumar826344@gmail.com>
2024-01-23 05:07:48 +0530

    fixed an error

--- PASSWORD GENERATOR.py
+++ PASSWORD GENERATOR.py
@@ -6,16 +6,21 @@ password_len=random.randrange(8,16)  #genrate the length of password between 8 t
 
 #genrates the password
 def passwod_genrator():
- global passw
- a=0
- for a in range(0,4):             #   
-  strr=(random.choice(string.ascii_lowercase),random.choice(string.ascii_uppercase),random.choice(string.digits),random.choice(string.punctuation))
-  passw=random.choice(strr[a])
-  s.append(passw)
-  a=a+1
+   global passw
+   a=0
+   #store character in s which are genrated randomly
+   for a in range(0,4):
+      strr=(random.choice(string.ascii_lowercase),random.choice(string.ascii_uppercase),random.choice(string.digits),random.choice(string.punctuation))
+      passw=random.choice(strr[a])
+      s.append(passw)
+      a=a+1
 
+
+#this loop add each character stored in s
 for i in range(0,password_len):
     passwod_genrator()
     password=password+str(s[i])
     i=i+1
+
+#print final password
 print("Passwod is:",password)
